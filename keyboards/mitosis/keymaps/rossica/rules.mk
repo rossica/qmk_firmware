a7e594bdc97116be3ea6eaee1aebbf66f45f6f43
rossica <8543330+rossica@users.noreply.github.com>
2024-05-02 01:53:32 -0700

    Clean up rules.mk

--- keyboards/mitosis/keymaps/rossica/rules.mk
+++ keyboards/mitosis/keymaps/rossica/rules.mk
@@ -1,9 +1,7 @@
-AUDIO_ENABLE = no # audio output
 FAUXCLICKY_ENABLE = no
-BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no
+COMMAND_ENABLE = no	# Commands for debug and configuration
 UNICODE_ENABLE = no	# Unicode
-BACKLIGHT_ENABLE = no	# Enable keyboard backlight functionality
 BLUETOOTH_ENABLE = no	# Enable Bluetooth with the Adafruit EZ-Key HID
 MIDI_ENABLE = no	# MIDI controls
 
