04f328d4c33d9bcccccf9ff2052b4a91f093fc1d
rossica <8543330+rossica@users.noreply.github.com>
2024-05-02 01:47:27 -0700

    Add shift to symbol layer

--- keyboards/mitosis/keymaps/rossica/keymap.c
+++ keyboards/mitosis/keymaps/rossica/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUM] = LAYOUT( /* Number layout along the home row for maximum speed*/
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_LSFT, KC_F11,  KC_GRV,  KC_EQL,  KC_LBRC,       KC_RBRC, KC_MINS, KC_QUOT, KC_F12,  KC_BSLS,
+    KC_LSFT, KC_F11,  KC_GRV,  KC_EQL,  KC_LBRC,       KC_RBRC, KC_MINS, KC_QUOT, KC_F12,  SFT_T(KC_BSLS),
              _______, _______, _______, _______,       _______, KC_HOME, KC_PGUP, KC_END,
              _______, _______, _______, _______,       _______, _______, KC_PGDN, _______
   ),
--- keyboards/mitosis/keymaps/rossica/rules.mk
+++ keyboards/mitosis/keymaps/rossica/rules.mk
@@ -2,7 +2,6 @@ AUDIO_ENABLE = no # audio output
 FAUXCLICKY_ENABLE = no
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no
-COMMAND_ENABLE = no	# Commands for debug and configuration
 UNICODE_ENABLE = no	# Unicode
 BACKLIGHT_ENABLE = no	# Enable keyboard backlight functionality
 BLUETOOTH_ENABLE = no	# Enable Bluetooth with the Adafruit EZ-Key HID
